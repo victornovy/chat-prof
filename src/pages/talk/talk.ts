b5efaca3aa3bbbfd83ec9ee0f12845a6bc04426c
Victor Novy <novyvictor@gmail.com>
2018-10-27 11:44:14 -0300

    Get messages

--- src/pages/talk/talk.ts
+++ src/pages/talk/talk.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TalkDetailPage } from '../talk-detail/talk-detail';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Timestamp } from 'rxjs';
 
 @IonicPage()
 @Component({
@@ -8,13 +10,63 @@ import { TalkDetailPage } from '../talk-detail/talk-detail';
     templateUrl: 'talk.html'
 })
 export class TalkPage {
-    constructor(public navCtrl: NavController, public navParams: NavParams) {}
+    title: string;
+    talkList: Array<any> = [];
+    mensagem: string;
 
-    ionViewDidLoad() {
-        console.log('ionViewDidLoad TalkPage');
+    constructor(
+        private navCtrl: NavController,
+        private navParams: NavParams,
+        _db: AngularFirestore
+    ) {
+        const info = this.navParams.get('info');
+        this.title = info.nome;
+
+        _db.collection(`grupos/${info.id}/msg`, ref =>
+            ref.orderBy('data', 'asc')
+        )
+            .valueChanges()
+            .subscribe(item => {
+                this.talkList = item.map((item: any) => {
+                    item.formatData = this.formatData(item.data.seconds);
+                    return item;
+                });
+            });
     }
 
     openTalkDetail() {
         this.navCtrl.push(TalkDetailPage);
+    }
+
+    showData(data) {
+        const min = this.zeroFillData(data.minutes);
+        const hours = this.zeroFillData(data.hours);
+
+        return `${hours}:${min}`;
+    }
+
+    private formatData(value?: number) {
+        const data = !!value ? new Date(value * 1000) : new Date();
+        return {
+            day: data.getDate(),
+            month: data.getMonth() + 1,
+            year: data.getFullYear(),
+            hours: data.getHours(),
+            minutes: data.getMinutes(),
+            seconds: data.getSeconds()
+        };
+    }
+
+    private zeroFillData(value) {
+        return value.toString().length === 2 ? value : `0${value}`;
+    }
+
+    sendMsg() {
+        this.talkList.push({
+            autor: 'batata',
+            texto: this.mensagem,
+            formatData: this.formatData()
+        });
+        this.mensagem = '';
     }
 }
